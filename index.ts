fbe8c2f986068a981e56ffcbc66f364de4792de6
Systemized <VincentNguyen.XLV@gmail.com>
2026-01-21 15:58:05 -0500

    added some chat/user features to index.ts

--- index.ts
+++ index.ts
@@ -1,31 +1,82 @@
+import type { Socket } from "bun";
+
 const HOST = "127.0.0.1";
 const PORT = 3000;
 
-const clients = new Set<any>();
+interface User {
+    username: string;
+    socket: any;
+}
+
+const clients = new Map<string, User>();
+const socketToUser = new Map<any, string>();
+
+const closeSocket = (sock: Socket) => {
+    const user = socketToUser.get(sock);
+    if (!user) return;
+
+    clients.delete(user);
+    socketToUser.delete(sock)
+    return user;
+}
 
 const server = Bun.listen({
     hostname: HOST,
     port: PORT,
 
     socket: {
-        open(socket) {
-            clients.add(socket);
-            socket.write("Client Connected\n\n");
+        open(sock) {
+            sock.write("Welcome to TCP Server. Enter a username: ")
         },
 
-        data(socket, data) {
+        data(sock: any, data: Buffer) {
+            const txt = data.toString().trim()
+            if (!txt) return;
+
+            // If username not found/ undefined, set user input as username
+            if (!socketToUser.get(sock)) {
+                if (clients.get(txt)) {
+                    sock.write("Username already taken. Enter a username: ")
+                    return;
+                };
+
+                console.log(`Enter - ${txt} joined the room`);
+                socketToUser.set(sock, txt);
+                clients.set(txt, { username: txt, socket: sock });
+                clients.forEach(client => {
+                    client.socket.write(`${txt} has joined the room. There are ${socketToUser.size} users online\n\n`)
+                })
+
+                return;
+            } else {
+                const user = socketToUser.get(sock)
+                
+                sock.write(`${user}: `)
+                console.log(`Recieved - ${user}: ${txt}`);
+                clients.forEach(client => {
+                    if (client.username !== user) {
+                        client.socket.write(`${user}: ${txt}\n`);
+                    }
+                });
+            }
+
+
+        },
+
+        close(sock) {
+            const user = closeSocket(sock);
+            
+            console.log(`Exit - ${user} left the room`);
             clients.forEach(client => {
-                client.write(`Message: ${data}`)
-            });
+                client.socket.write(`${user} has left the room. There are ${socketToUser.size} users online\n`)
+            })
         },
 
-        close(socket) {
-            clients.delete(socket)
-        },
-        error(socket) {
-            clients.delete(socket)
-        },
-    },
-});
+        error(sock, err) {
+            closeSocket(sock);
+            console.error(`${sock} Error - ${err.message}`);
+        }
+    }
+})
 
-console.log(`Starting TCP Server at ${HOST}:${PORT}`);
+console.log(`TCP Server is running at ${HOST}:${PORT}`);
