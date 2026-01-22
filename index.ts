cdbd0e31244b4866271a6182c589899e1c5ac046
Systemized <VincentNguyen.XLV@gmail.com>
2026-01-22 10:20:58 -0500

    improved type safety, small fixes

--- index.ts
+++ index.ts
@@ -5,11 +5,11 @@ const PORT = 3000;
 
 interface User {
     username: string;
-    socket: any;
+    socket: Socket;
 }
 
 const clients = new Map<string, User>();
-const socketToUser = new Map<any, string>();
+const socketToUser = new Map<Socket, string>();
 
 const closeSocket = (sock: Socket) => {
     const user = socketToUser.get(sock);
@@ -26,19 +26,24 @@ const server = Bun.listen({
 
     socket: {
         open(sock) {
-            sock.write("Welcome to TCP Server. Enter a username: ")
+            sock.write("Welcome to TCP Server. Enter a username:\n")
         },
 
-        data(sock: any, data: Buffer) {
+        data(sock: Socket, data: Buffer) {
             const txt = data.toString().trim()
             if (!txt) return;
 
-            // If username not found/ undefined, set user input as username
+            // If user's socket not found, set user input as username and map sockets/username
             if (!socketToUser.get(sock)) {
                 if (clients.get(txt)) {
-                    sock.write("Username already taken. Enter a username: ")
+                    sock.write("Username already taken. Enter a username:\n")
                     return;
                 };
+
+                if (txt.length > 16) {
+                    sock.write("Username must be under 16 Chars. Enter a username:\n")
+                    return;
+                }
 
                 console.log(`Enter - ${txt} joined the room`);
                 socketToUser.set(sock, txt);
@@ -50,8 +55,8 @@ const server = Bun.listen({
                 return;
             } else {
                 const user = socketToUser.get(sock)
-                
-                sock.write(`${user}: `)
+                if (!user) return;
+
                 console.log(`Recieved - ${user}: ${txt}`);
                 clients.forEach(client => {
                     if (client.username !== user) {
@@ -63,7 +68,7 @@ const server = Bun.listen({
 
         },
 
-        close(sock) {
+        close(sock: Socket) {
             const user = closeSocket(sock);
             
             console.log(`Exit - ${user} left the room`);
@@ -72,7 +77,7 @@ const server = Bun.listen({
             })
         },
 
-        error(sock, err) {
+        error(sock: Socket, err: Error) {
             closeSocket(sock);
             console.error(`${sock} Error - ${err.message}`);
         }
